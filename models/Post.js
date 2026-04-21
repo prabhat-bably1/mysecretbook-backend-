9e11ceba8e7cbda3cc0654894f2d8709409e992b
prabhat-bably1 <prabhatr.seth04@gmail.com>
2026-04-21 12:11:23 +0530

    Create Post model with schema definition

---
+++ models/Post.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  secretId: {
+    type: String,
+    default: ""
+  },
+  email: {
+    type: String,
+    default: ""
+  },
+  phone: {
+    type: String,
+    default: ""
+  },
+  type: {
+    type: String,
+    enum: ["word", "image"],
+    required: true
+  },
+  text: {
+    type: String,
+    default: ""
+  },
+  imageUrl: {
+    type: String,
+    default: ""
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("Post", postSchema);
